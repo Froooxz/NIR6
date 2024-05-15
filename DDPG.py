f5a831164c35c175bbc8bb8c91084e32329218e1
Froooxz <Froooxz@yandex.ru>
2024-05-15 23:57:34 +0300

    Кастомная политика

--- DDPG.py
+++ DDPG.py
@@ -15,18 +15,18 @@ from HeatingEnv import HeatingEnv
 
 import torch.nn as nn
 
-class CustomMLP(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int, n_critics):
-        super(CustomMLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 64)  # Первый скрытый слой с 64 нейронами
-        self.fc2 = nn.Linear(64, 64)         # Второй скрытый слой с 64 нейронами
-        self.fc3 = nn.Linear(64, output_dim) # Выходной слой с заданным числом выходных нейронов
-
-    def forward(self, x):
-        x = nn.functional.relu(self.fc1(x))  # Применение линейной активации ReLU к первому скрытому слою
-        x = nn.functional.relu(self.fc2(x))  # Применение линейной активации ReLU ко второму скрытому слою
-        x = self.fc3(x)                      # Получение выхода
-        return x
+# class CustomMLP(nn.Module):
+#     def __init__(self, input_dim: int, output_dim: int):
+#         super(CustomMLP, self).__init__()
+#         self.fc1 = nn.Linear(input_dim, 64)  # Первый скрытый слой с 64 нейронами
+#         self.fc2 = nn.Linear(64, 64)         # Второй скрытый слой с 64 нейронами
+#         self.fc3 = nn.Linear(64, output_dim) # Выходной слой с заданным числом выходных нейронов
+#
+#     def forward(self, x):
+#         x = nn.functional.relu(self.fc1(x))  # Применение линейной активации ReLU к первому скрытому слою
+#         x = nn.functional.relu(self.fc2(x))  # Применение линейной активации ReLU ко второму скрытому слою
+#         x = self.fc3(x)                      # Получение выхода
+#         return x
 
 
 env = HeatingEnv()
@@ -35,7 +35,7 @@ env = DummyVecEnv([lambda: env])
 
 # Обучение с использованием DDPG
 model = DDPG(
-    policy=CustomMLP,  # используем MlpPolicy в качестве политики
+    policy=MlpPolicy,  # используем MlpPolicy в качестве политики
     env=env,  # среда обучения
     # learning_rate=0.01,  # скорость обучения
     # buffer_size=1000000,  # размер буфера воспроизведения
@@ -43,7 +43,7 @@ model = DDPG(
     # learning_starts=1000,  # обучение после выполнения X шагов
     # train_freq=1,  # обновляем политику каждые X шагов
     # gradient_steps=10,  # количество шагов градиентного спуска для обновления политики
-    # policy_kwargs=dict(n_critics=1),  # Пример настройки архитектуры нейронной сети политики
+    policy_kwargs=dict(net_arch=[4]),  # Пример настройки архитектуры нейронной сети политики
     verbose=1  # информацию о ходе обучения
 )
 
